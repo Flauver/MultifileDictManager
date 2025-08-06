cdb8459b17871cf647c3fba33efb2aa88ab22116
Flauver <thedarkwhite@126.com>
2025-08-06 14:40:30 +0800

    新增自动补充占位文字的功能

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,33 @@ fn main() {
     for 文件名 in 所有文件名 {
         let mut lines = Vec::new();
         for line in fs::read_to_string("data/".to_owned() + &文件名).unwrap().lines() {
-            lines.push(line.split('\t').map(|x| x.to_string()).collect::<Vec<_>>())
+            lines.push(line.split('\t')
+                .map(|x| x.to_string())
+                .collect::<Vec<_>>());
         }
         for entry in lines.iter().filter(|x| x.len() == 2) {
-            码表.entry(entry[1].clone()).or_insert(Vec::new()).push(entry[0].clone());
+            if let Some(候选) = 码表.entry(entry[1].clone())
+                .or_insert(Vec::<Option<String>>::new())
+                .iter_mut()
+                .find(|x| x.is_none()) {
+                *候选 = Some(entry[0].clone());
+            } else {
+                码表.get_mut(&entry[1]).unwrap().push(Some(entry[0].clone()));
+            }
         }
         for entry in lines.iter().filter(|x| x.len() == 3) {
             let 词 = entry[0].clone();
             let 码 = entry[1].clone();
             if let Ok(候选位) = entry[2].parse::<usize>() {
-                码表.entry(码).or_insert(Vec::new()).insert(候选位 - 1, 词);
+                let 候选 = 码表.entry(码.clone()).or_insert(Vec::new());
+                while 候选位 - 1 > 候选.len() {
+                    候选.push(None);
+                }
+                if 候选.get(候选位 - 1).unwrap_or(&Some(String::new())).is_none() {
+                    码表.get_mut(&码).unwrap()[候选位 - 1] = Some(词);
+                } else {
+                    码表.get_mut(&码).unwrap().insert(候选位 - 1, Some(词));
+                }
             }
         }
         for entry in lines.iter().filter(|x| x.len() == 4 || (x.len() == 3 && x[2].parse::<usize>().is_err())) {
@@ -31,14 +48,24 @@ fn main() {
                 let 词 = entry[0].clone();
                 let 码 = entry[1].clone();
                 let 候选位 = entry[2].parse::<usize>().unwrap();
-                let mut 新候选 = 码表[&码].clone().into_iter().filter(|x| *x != 词).collect::<Vec<_>>();
-                新候选.insert(候选位 - 1, 词);
+                let mut 新候选 = 码表[&码].clone()
+                    .into_iter()
+                    .filter(|x| *x != Some(词.clone()))
+                    .collect::<Vec<_>>();
+                while 候选位 - 1 > 新候选.len() {
+                    新候选.push(None);
+                }
+                if 新候选[候选位 - 1].is_none() {
+                    码表.get_mut(&码).unwrap()[候选位 - 1] = Some(词);
+                } else {
+                    码表.get_mut(&码).unwrap().insert(候选位 - 1, Some(词));
+                }
                 码表.insert(码, 新候选);
             } else {
                 if entry[2] == "删" {
-                    码表.get_mut(&entry[1]).unwrap().retain(|x| *x != entry[0]);
+                    码表.get_mut(&entry[1]).unwrap().retain(|x| *x != Some(entry[0].clone()));
                 } else {
-                    码表.insert(entry[0].clone(), entry[1].split(" ").map(|x| x.to_string()).collect::<Vec<_>>());
+                    码表.insert(entry[0].clone(), entry[1].split(" ").map(|x| Some(x.to_string())).collect::<Vec<_>>());
                 }
             }
         }
@@ -46,7 +73,19 @@ fn main() {
 
     let mut 码表 = 码表.into_iter().collect::<Vec<_>>();
     码表.sort();
-    码表.retain(|x| !x.1.is_empty());
+    码表.retain(|x| !x.1.is_empty() && !x.1.iter().all(|x| x.is_none()));
+    let 码表 = 码表.into_iter().map(|(码, 词)| {
+        let mut 候选 = Vec::new();
+        for (i, 词) in 词.into_iter().enumerate() {
+            if let Some(词) = 词 {
+                候选.push(词);
+            } else {
+                println!("已自动补充编码 {} 的第 {} 项的占位文字", 码, i + 1);
+                候选.push(format!("候选{}", i + 1));
+            }
+        }
+        (码, 候选)
+    }).collect::<Vec<_>>();
 
     println!("请选择你要导出的码表格式：");
     println!("1.多多格式（词tab码）");
@@ -85,4 +124,5 @@ fn main() {
         }
     }
     fs::write("词库.txt", output.join("\n")).unwrap();
+    let _ = std::io::stdin().read_line(&mut mode);
 }
